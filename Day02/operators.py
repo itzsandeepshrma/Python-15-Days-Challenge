6090649c6d44a844a6fdc66d9a6562c1a0cfa8b8
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-01-10 20:17:51 +0530

    Update operators.py update successfully

--- Day02/operators.py
+++ Day02/operators.py
@@ -11,3 +11,26 @@ b = 10
 # +=5 Increment the value of b by 5 then assign it to b 
 b -= 5 # Decrement the value of b by 5 then assign it to b
 print(b)
+
+# Comparison Operators 
+
+d = 5==5
+print(d)
+
+e = True or False
+print(e)
+
+# Truth table of "or"
+print("True or False id ",True or False)
+print("True or True id ",True or True)
+print("False or False id ",False or False)
+print("True or True id ",True or True)
+
+# Truth table of "and"
+print("True and False id ",True and False)
+print("True and True id ",True and True)
+print("False and False id ",False and False)
+print("True and True id ",True and True)
+
+print(not(True))
+print(not(False))
