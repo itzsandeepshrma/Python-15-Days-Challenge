64908ab431b5bec836032d280be55386a0fed00f
𝚂αɴɖεερ 𝚂ʜαяɱα <sandeepshrmaxcoder@gmail.com>
2025-01-10 17:03:28 +0530

    Create operators.py update soon

---
+++ Day02/operators.py
@@ -0,0 +1,13 @@
+# Arithmetiic Operators
+a = 45
+b = 54 
+c = a*b
+print(c)
+
+# Assignment Operators 
+a = 18-9 # Assign 18-9 in a 
+print(a)
+b = 10
+# +=5 Increment the value of b by 5 then assign it to b 
+b -= 5 # Decrement the value of b by 5 then assign it to b
+print(b)
